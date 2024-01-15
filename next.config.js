e3e4970e2f601d68d07987cecb86920de3d55b30
NguyenKimDai <kimdai2601@gmail.com>
2024-01-15 22:06:18 +0700

    re CI/CD

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  sentry: {
+    hideSourceMaps: true,
+    disableServerWebpackPlugin: false,
+    disableClientWebpackPlugin: false,
+  },
 };
 
 module.exports = nextConfig;
