44041fd2be3f9134089cb345d6133dcfb2ec0704
NguyenKimDai <kimdai2601@gmail.com>
2024-02-17 10:46:42 +0700

    add author name in search enable search by author name

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,7 +1,10 @@
+import { Author } from "./author";
+
 export type Book = {
   id: number;
   bookName: string;
   enabled: boolean;
   createdAt: string;
   updatedAt: string;
+  author?: Author;
 };
